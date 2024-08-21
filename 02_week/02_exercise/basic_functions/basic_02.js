6ec4fb16b73f5a516007f1964216836f889ae7dd
Akram Daud <s2400776@BC-C02GH13VQ6LR-MacBook-Air.local>
2024-08-21 12:28:06 +0300

    first commit

---
+++ 02_week/02_exercise/basic_functions/basic_02.js
@@ -0,0 +1,8 @@
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+
+function mul(a,b){
+    return(a*b);
+}
+// sample usage - do not modify
+console.log(mul(2, 4));
+console.log(mul(3, 2));
